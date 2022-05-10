1967e5a1e855b372fde787bc7d21e6436e86cfcd
gkatzioura <gkatzioura@gmail.com>
2022-05-10 21:21:31 +0100

    Added payment for api

---
+++ yapily-rest-smr-2022/src/main/java/com/yapily/rest/smr/config/YapilyConfiguration.java
@@ -0,0 +1,37 @@
+package com.yapily.rest.smr.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.yapily.sdk.ApiClient;
+import com.yapily.sdk.api.AuthorisationsApi;
+import com.yapily.sdk.api.PaymentsApi;
+import com.yapily.sdk.auth.HttpBasicAuth;
+import com.yapily.sdk.model.PaymentAuthorisationRequest;
+
+import io.swagger.annotations.Api;
+
+@Configuration
+public class YapilyConfiguration {
+
+    @Bean
+    public ApiClient apiClient(@Value("${yapily.api.key}") String key, @Value("${yapily.api.secret}") String secret) {
+        ApiClient defaultClient = new ApiClient();
+        defaultClient.setUsername(key);
+        defaultClient.setPassword(secret);
+        defaultClient.setBasePath("https://api.yapily.com");
+        return defaultClient;
+    }
+
+    @Bean
+    public AuthorisationsApi authorisationsApi(ApiClient apiClient) {
+        return new AuthorisationsApi(apiClient);
+    }
+
+    @Bean
+    public PaymentsApi paymentsApi(ApiClient apiClient) {
+        return new PaymentsApi(apiClient);
+    }
+
+}
---
+++ yapily-rest-smr-2022/src/main/java/com/yapily/rest/smr/payment/PaymentController.java
@@ -0,0 +1,39 @@
+package com.yapily.rest.smr.payment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yapily.sdk.api.AuthorisationsApi;
+import com.yapily.sdk.api.PaymentsApi;
+import com.yapily.sdk.model.ApiResponseOfPaymentAuthorisationRequestResponse;
+import com.yapily.sdk.model.PaymentAuthorisationRequest;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private AuthorisationsApi authorisationsApi;
+
+    @Autowired
+    private PaymentsApi paymentsApi;
+
+    @GetMapping("/{id}/status")
+    public Mono<String> retrieve(@PathVariable String id) {
+        return Mono.just(id);
+    }
+
+    @PostMapping("/")
+    public Mono<ApiResponseOfPaymentAuthorisationRequestResponse> create(@RequestBody PaymentAuthorisationRequest paymentAuthorisationRequest) {
+        return authorisationsApi.createPaymentAuthorisation(paymentAuthorisationRequest,null, null,null,false);
+    }
+
+
+}
