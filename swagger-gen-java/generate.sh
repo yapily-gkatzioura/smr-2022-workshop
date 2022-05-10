1b7d2a004983902797f61ba91543e177ad13c993
gkatzioura <gkatzioura@gmail.com>
2022-05-10 18:54:37 +0100

    Generated Yapily Sdk

--- swagger-gen-java/generate.sh
+++ swagger-gen-java/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npx @openapitools/openapi-generator-cli generate -i https://api.yapily.com/docs/v3/openapi.json -g java -o ./
+npx @openapitools/openapi-generator-cli generate -i https://api.yapily.com/docs/v3/openapi.json -g java -c ./config.json -o ./yapily-sdk
